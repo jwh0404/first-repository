e2d7ed999f394cb1ce57356b4eb82afc50e24520
cloudstudyJ <cloudstudyJ@gmail.com>
2024-09-11 15:37:53 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,11 @@
+def fibo(a):
+    if (a <= 2):
+        return 1
+
+    return fibo(a - 1) + fibo(a - 2)
+
 def print_hello():
-    print("Hello World!")
+    print("Hello World!!")
 
 if __name__ == "__main__":
-    print_hello()
+    print(fibo(10))
